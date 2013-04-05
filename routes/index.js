f3b5559177e1c3adec6307836fb4a0d36c8991d3
william <wwwy3y3@gmail.com>
2013-04-05 17:53:47 +0800

    prototype

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,29 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: '成大成景之夜' });
+  res.render('index', { title: '成大成景之夜', nav: 'index' });
 };
 
 exports.specialthx = function(req, res){
-  res.render('specialthx', { title: '特別感謝' });
+  res.render('specialthx', { title: '特別感謝', nav: 'specialthx' });
 };
 
 exports.contacts = function(req, res){
-  res.render('contacts', { title: '聯絡我們' });
+  res.render('contacts', { title: '聯絡我們', nav: 'contacts' });
 };
 
 exports.actors = function(req, res){
-  res.render('actors', { title: '劇組名單	' });
+  res.render('actors', { title: '劇組名單	', nav: 'actors' });
 };
 
 exports.guests = function(req, res){
-  res.render('guests', { title: '特別來賓' });
+  res.render('guests', { title: '特別來賓', nav: 'guests' });
 };
 
 exports.sponsors = function(req, res){
-  res.render('sponsors', { title: '贊助廠商' });
+  res.render('sponsors', { title: '贊助廠商', nav: 'sponsors' });
 };
 
 exports.compaign = function(req, res){
-  res.render('compaign', { title: '宣傳' });
+  res.render('compaign', { title: '宣傳', nav: 'compaign' });
 };
